8f2461e7e41219a4e910867010cfa18738dba862
Fred Underwood <underwoodfred@proton.me>
2025-06-20 13:16:03 +1000

    rm wifi files

--- device.mk
+++ device.mk
@@ -400,15 +400,6 @@ $(call soong_config_set,google_displaycolor,displaycolor_platform,gs101)
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
-# WPA SUPPLICANT
-PRODUCT_COPY_FILES += \
-	device/google/gs101/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
-	device/google/gs101/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
-
-# WIFI COEX
-PRODUCT_COPY_FILES += \
-	device/google/gs101/wifi/coex_table.xml:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/coex_table.xml
-
 PRODUCT_PACKAGES += hostapd
 PRODUCT_PACKAGES += wpa_supplicant
 PRODUCT_PACKAGES += wpa_supplicant.conf
