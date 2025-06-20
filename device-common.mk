893f4da07c22f51eaac6894489bd465bfc7b4cb9
Fred Underwood <underwoodfred@proton.me>
2025-06-21 08:05:46 +1000

    rm soc properties

--- device-common.mk
+++ device-common.mk
@@ -35,10 +35,6 @@ endif
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
-# Set system properties identifying the chipset
-PRODUCT_VENDOR_PROPERTIES += ro.soc.manufacturer=Google
-PRODUCT_VENDOR_PROPERTIES += ro.soc.model=Tensor
-
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.testing_battery_profile=2
 
