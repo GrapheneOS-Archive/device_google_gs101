d34c7c26384c030af7470a71b250491985f0e578
Fred Underwood <underwoodfred@proton.me>
2025-06-20 13:48:18 +1000

    rm proprietay gchips soong namespaces

--- device.mk
+++ device.mk
@@ -553,16 +553,6 @@ endif
 include device/google/gs-common/edgetpu/edgetpu.mk
 # Config variables for TPU chip on device.
 $(call soong_config_set,edgetpu_config,chip,abrolhos)
-# Include the edgetpu targets defined the namespaces below into the final image.
-PRODUCT_SOONG_NAMESPACES += \
-	vendor/google_devices/gs101/proprietary/gchips/tpu/metrics \
-	vendor/google_devices/gs101/proprietary/gchips/tpu/tflite_delegate \
-	vendor/google_devices/gs101/proprietary/gchips/tpu/darwinn_logging_service \
-	vendor/google_devices/gs101/proprietary/gchips/tpu/nnapi_stable_aidl \
-	vendor/google_devices/gs101/proprietary/gchips/tpu/aidl \
-	vendor/google_devices/gs101/proprietary/gchips/tpu/hal \
-	vendor/google_devices/gs101/proprietary/gchips/tpu/tachyon/tachyon_apis \
-	vendor/google_devices/gs101/proprietary/gchips/tpu/tachyon/service
 # TPU firmware
 PRODUCT_PACKAGES += edgetpu-abrolhos.fw
 
