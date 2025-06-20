5f0ab87a5084235c2f718ccb125fa528789a0a0b
Fred Underwood <underwoodfred@proton.me>
2025-06-21 07:37:20 +1000

    rm vendor.pktrouter property

--- telephony/pktrouter.mk
+++ telephony/pktrouter.mk
@@ -1,4 +1,3 @@
-PRODUCT_PROPERTY_OVERRIDES += vendor.pktrouter=1
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs101-sepolicy/telephony/pktrouter
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     $(TARGET_COPY_OUT_SYSTEM)/bin/oem-iptables-init.sh
