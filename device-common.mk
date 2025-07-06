GrapheneOS/device_google_gs201

add privapp-permissions for high brightness mode manager

device-common.mk
@@ -31,6 +31,9 @@ PRODUCT_COPY_FILES += \
     device/google/gs201/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
 endif
 
+PRODUCT_COPY_FILES += \
+    device/google/gs201/system_ext-permissions-stock.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/system_ext-permissions-stock.xml
+
 # Android Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml

GrapheneOS-Archive/device_google_gs101

device-common.mk
@@ -31,6 +31,9 @@ PRODUCT_COPY_FILES += \
     device/google/gs101/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
 endif
 
+PRODUCT_COPY_FILES += \
+    device/google/gs101/system_ext-permissions-stock.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/system_ext-permissions-stock.xml
+
 # Android Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml